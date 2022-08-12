0ebb53caf7f689978b0f116a10fb9fcee1cc4489
joseph <57895391+josephsnow26@users.noreply.github.com>
2022-08-12 02:06:26 +0100

    Update python.py

--- python.py
+++ python.py
@@ -1,44 +1,24 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
-import time as time
-import toml as toml
-import _mysql_connector
+import mysql.connector
 
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
 
+conn = init_connection()
 
-toml_data = toml.load("secrets.toml")
-# saving each credential into a variable
-HOST_NAME = toml_data['mysql']['host']
-DATABASE = toml_data['mysql']['database']
-PASSWORD = toml_data['mysql']['password']
-USER = toml_data['mysql']['user']
-PORT = toml_data['mysql']['port']
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
-mydb = connection.connect(host=HOST_NAME, database=DATABASE, user=USER, passwd=PASSWORD, use_pure=True)
-query = pd.read_sql('SELECT * FROM Client;' , mydb)
+rows = run_query("SELECT * from mytable;")
 
-
-
-# import streamlit as st
-# import numpy as np
-# import time as time
-# progress_bar = st.sidebar.progress(0)
-# status_text = st.sidebar.empty()
-# last_rows = np.random.randn(1, 1)
-# chart = st.line_chart(last_rows)
-
-# for i in range(1, 101):
-#     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-#     status_text.text("%i%% Complete" % i)
-#     chart.add_rows(new_rows)
-#     progress_bar.progress(i)
-#     last_rows = new_rows
-#     time.sleep(0.05)
-
-# progress_bar.empty()
-
-# # Streamlit widgets automatically run the script from top to bottom. Since
-# # this button is not connected to any other logic, it just causes a plain
-# # rerun.
-# st.button("Re-run")
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
